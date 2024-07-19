3bef437ad3bc23f60cddd83ee87fb37aafd32f79
Volkov <volkovyegorandriiovich@gmail.com>
2024-07-19 23:26:29 +0300

    solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,19 @@
-# Write your code here
+$RegionFiles = Get-ChildItem .\data\
+$list = @()
+
+foreach ($RegionFile in $RegionFiles)
+{
+  $Sizes = Get-Content $RegionFile -Raw | ConvertFrom-Json
+
+  foreach ($Size in $Sizes)
+    {
+      if ($Size.Name -eq "Standard_B2pts_v2")
+        {
+          $RegionName = $RegionFile.BaseName
+          $list += $RegionName
+        }
+    }
+}
+
+$JsonList = ConvertTo-Json -InputObject $list
+Set-Content -Path ".\result.json" -Value $JsonList
